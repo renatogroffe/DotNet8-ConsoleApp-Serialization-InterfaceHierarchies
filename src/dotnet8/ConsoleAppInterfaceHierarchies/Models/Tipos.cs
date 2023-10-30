16e04a49a19a6e8a251be0c939830762cfba310a
Renato Groffe <renato.groffe@yahoo.com.br>
2023-10-30 20:59:12 -0300

    .NET 8 + Console Application + Serialização com hierarquias de interfaces
    .NET 8 + Console Application + Serialização com hierarquias de interfaces

---
+++ src/dotnet8/ConsoleAppInterfaceHierarchies/Models/Tipos.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleAppInterfaceHierarchies.Models;
+
+public interface IContinente
+{
+    public string? NomeContinente { get; set; }
+}
+
+public interface IPais : IContinente
+{
+    public string? NomePais { get; set; }
+}
+
+public interface ICidade : IPais
+{
+    public string? NomeCidade { get; set; }
+}
+
+public class Localidade : ICidade
+{
+    public string? NomeContinente { get; set; }
+    public string? NomePais { get; set; }
+    public string? NomeCidade { get; set; }
+}
---
+++ src/dotnet8/ConsoleAppInterfaceHierarchies/Program.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleAppInterfaceHierarchies.Models;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+Console.WriteLine("***** Testes com .NET 8 | Serializacao usando Interface Hierarchies *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+
+var localidades = new Localidade[]
+{
+    new () { NomeCidade = "Sao Paulo", NomePais = "Brasil", NomeContinente = "America do Sul" },
+    new () { NomeCidade = "Roma", NomePais = "Italia", NomeContinente = "Europa" },
+    new () { NomeCidade = "Toquio", NomePais = "Japao", NomeContinente = "Asia" }
+};
+
+Console.WriteLine();
+Console.WriteLine("*** Resultados da serializacao usando as interfaces IContinente, IPais e ICidade ***");
+foreach (var cidade in localidades)
+{
+    Console.WriteLine();
+    ExibirInfoLocalidade<IContinente>(cidade);
+    ExibirInfoLocalidade<IPais>(cidade);
+    ExibirInfoLocalidade<ICidade>(cidade);
+}
+
+static void ExibirInfoLocalidade<T>(T cidade) =>
+    Console.WriteLine($"{typeof(T).Name} = {JsonSerializer.Serialize<T>(cidade)}");
